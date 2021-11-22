a4261f2e32bdc213c055a9753e8703c2fe12b5a8
LuchoDev <luiswblancod@gmail.com>
2021-11-22 01:08:33 -0500

    navbar-responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,27 @@
-const texto = document.getElementById('text');
+const navMenu = document.getElementById('nav-menu');
+const toggleMenu = document.getElementById('nav-toggle');
+const closeMenu = document.getElementById('nav-close');
 
-texto.textContent = "NavBar paso a paso";
+/* SHOW */
+toggleMenu.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+});
+
+/* HIDDEN */
+closeMenu.addEventListener('click', () => {
+    navMenu.classList.remove('show');
+});
+
+/* ======= ACTIVE AND REMOVE MENU ======= */
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+    // Active link
+    navLink.forEach(n => n.classList.remove('active'));
+    this.classList.add('active');
+
+    // Remove menu mobile
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
